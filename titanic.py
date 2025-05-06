12d58ae286f2b698e67967611d6637f5b2a35d3d
Rohan Chaudhari <98477997+Orion1163@users.noreply.github.com>
2025-05-06 23:41:18 +0530

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -14,7 +14,13 @@ def map_reduce_with_pandas(input_file):
     deceased_females_by_class = df[(df['Survived'] == 0) & (df['Sex'] == 'female')]
 
     # Reduce: Count deceased females by class
-    count_deceased_females_by_class = deceased_females_by_class['Class'].value_counts()
+    count_deceased_females_by_class = deceased_females_by_class['Pclass'].value_counts()
 
     return average_age_deceased_males, count_deceased_females_by_class
                                       
+                                      
+average_age, female_class_count = map_reduce_with_pandas("titanic_dataset.csv")
+
+print(f"Average age of deceased males: {average_age:.2f}")
+print("Number of deceased females in each class:")
+print(female_class_count)
